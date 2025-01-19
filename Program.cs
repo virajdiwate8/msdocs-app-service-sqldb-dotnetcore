69cbc8baa079e50f7321e23824ff077c128ac836
Viraj Diwate <virajdiwate8@gmail.com>
2025-01-19 14:16:07 +0000

    disable redis cache changes

--- Program.cs
+++ Program.cs
@@ -11,13 +11,9 @@ if(builder.Environment.IsDevelopment())
 }
 else
 {
-    builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-    builder.Services.AddStackExchangeRedisCache(options =>
-    {
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options.InstanceName = "SampleInstance";
-    });
+     builder.Services.AddDbContext<MyDatabaseContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+    builder.Services.AddDistributedMemoryCache();
 }
 
 
